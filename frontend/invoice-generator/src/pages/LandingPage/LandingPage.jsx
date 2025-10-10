fbdc1ec7c8324196af86f2dc2434b72a95b0e35d
Tharindu Lakshan <167046569+KADTLAKSHAN@users.noreply.github.com>
2025-10-11 01:13:16 +0530

    Create Hero component

--- frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
+++ frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,13 @@
 import Header from "../../components/landing/Header";
+import Hero from "../../components/landing/Hero";
 
 const LandingPage = () => {
   return (
     <div className="bg-[#ffffff] text-gray-600">
       <Header />
+      <main>
+        <Hero />
+      </main>
     </div>
   );
 };
