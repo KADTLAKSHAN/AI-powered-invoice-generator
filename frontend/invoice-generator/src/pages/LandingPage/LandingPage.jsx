79cfb0ea9a721cb50c679649801eb8bba3fc8ee0
Tharindu Lakshan <167046569+KADTLAKSHAN@users.noreply.github.com>
2025-10-16 12:01:18 +0530

    Create Testimonials component

--- frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
+++ frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import Features from "../../components/landing/Features";
 import Header from "../../components/landing/Header";
 import Hero from "../../components/landing/Hero";
+import Testimonials from "../../components/landing/Testimonials";
 
 const LandingPage = () => {
   return (
@@ -9,6 +10,7 @@ const LandingPage = () => {
       <main className="mb-[100vh]">
         <Hero />
         <Features />
+        <Testimonials />
       </main>
     </div>
   );
