f5caf093cccf713e837c48ca00bfd0dacad0c09b
Tharindu Lakshan <167046569+KADTLAKSHAN@users.noreply.github.com>
2025-10-17 16:07:01 +0530

    Backend Setup and Project Structure

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cores");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Connect to the database
+connectDB();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Define routes
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
