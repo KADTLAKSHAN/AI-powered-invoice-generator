937fc84dd798d43e664ff12cd4663360c26bae19
Tharindu Lakshan <167046569+KADTLAKSHAN@users.noreply.github.com>
2025-10-18 01:14:30 +0530

    connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cores");
+const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
